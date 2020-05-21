0a872463817e580cb75c784bbd61d52dfd6f2d97
nicholasm00 <nicholas.ma.1@vanderbilt.edu>
2020-05-21 14:24:35 -0500

    install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import axios from "axios";
 
 export default function App() {
+  axios.get('https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports')
+  .then((response) => {
+    console.log(response);
+  });
+
   return (
     <div>hello world</div>
   );
